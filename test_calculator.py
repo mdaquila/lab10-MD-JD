0806ef9aaaf4ed914b15757da378dc78b9d7d97a
fgeqma <fgeqma@gmail.com>
2025-04-09 15:39:03 -0400

    mute

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,8 @@
 import unittest
 from calculator import *
 
+# Partner 1: Matthew D'Aquila
+
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
@@ -11,12 +13,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(-3, 4), -12)
+        self.assertEqual(mul(0, 5), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(9, 3), 3)
+        with self.assertRaises(ZeroDivisionError):
+            divide(10, 0)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,23 +40,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(10, -5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-9)
+        self.assertEqual(square_root(9), 3)
+        self.assertEqual(square_root(16), 4)
 
-# Do not touch this
 if __name__ == "__main__":
     unittest.main()
